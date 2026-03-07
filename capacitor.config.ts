b0dc0a192700a5636047d31097049becdf757cb2
saiwoontip <tips01@udayton.edu>
2026-03-07 18:12:27 -0500

    fix: Use capacitor.config.json for better CI compatibility

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.dailydriver.app',
-  appName: 'Daily Driver',
-  webDir: 'dist'
-};
-
-export default config;
